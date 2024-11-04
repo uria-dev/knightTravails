56d38b88bdf502ab09d3be459dc7380b378527e4
Uria <dorman010@gmail.com>
2024-11-04 23:56:58 +0000

    reworked pathfinding logic to be more concise, started on knightMove logic

--- node.js
+++ node.js
@@ -1,11 +1,11 @@
 export class Node {
-  constructor(location) {
+  constructor(location, pastMoves = []) {
+    this.pastMoves = pastMoves;
     this.legalMoves = [];
     this.location = location;
     this.calculateMoves(location);
-    this.visited = false;
   }
-  calculateMoves(currentLocation = this.location) {
+  calculateMoves() {
     const possibleMoves = [
       [2, 1],
       [2, -1],
@@ -17,14 +17,28 @@ export class Node {
       [-1, -2],
     ];
     for (let i = 0; i < possibleMoves.length; i++) {
-      let move = [
-        currentLocation[0] + possibleMoves[i][0],
-        currentLocation[1] + possibleMoves[i][1],
-      ];
-      if (move[0] >= 0 && move[1] >= 0) {
-        this.legalMoves.push(move);
+      let x = this.location[0] + possibleMoves[i][0];
+      let y = this.location[1] + possibleMoves[i][1];
+
+      if (x >= 0 && x < 8) {
+        if (y >= 0 && y < 8) {
+          this.legalMoves.push([x, y]);
+        }
       }
     }
-    console.log(this.legalMoves);
+    if (this.legalMoves.length == 0) {
+      return;
+    }
+    this.pastMoves.push(this.location);
+    return this.legalMoves;
+  }
+  knightMove(target, moves = this.legalMoves) {
+    if (target[0] == this.location[0] && target[1] == this.location[1]) {
+      console.log(`Reached in ${this.pastMoves.length - 1} hops:`);
+      this.pastMoves.forEach((move) => {
+        console.log(move);
+        return;
+      });
+    }
   }
 }
